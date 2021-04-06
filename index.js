34b6c7624de69f155a56944b060168bad6d60bd0
james <jameshoggard.dev@gmail.com>
2021-04-06 16:25:03 -0400

    commit

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const header = document.querySelector("#header")
 console.log("Here's your header:", header)
 
 
-/***** Deliverable 2 *****/
 
+/***** Deliverable 2 *****/
+const newHeader = document.querySelector("#header")
+console.log(newHeader.style.color = 'blue')
 
 
 /***** Deliverable 3 *****/
 // console.log('This is what the traveler object looks like: ', traveler)
+
 
 
 /***** Deliverable 4 *****/
